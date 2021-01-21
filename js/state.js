50ecf6b06ce8e11bf57c5274b13b9fc0c36b6b79
Laura Pech <laura.pech@unitedprint.com>
2021-01-21 11:30:53 +0100

    initial commit

---
+++ js/state.js
@@ -0,0 +1,13 @@
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    render() { return (
+            <div>
+                <h2>
+                    It is {this.state.date.toLocaleTimeString()}.
+                </h2>
+            </div>
+    );}
+}
